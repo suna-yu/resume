734f20abdba610778c038be7ef5790b671290c7f
suna-yu <suna99_yu@naver.com>
2024-03-28 14:24:05 +0900

    ver.1.3.1 pj3 이미지 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// 로고 로티
+const lottie = bodymovin.loadAnimation({
+    container: $('header #logo')[0],
+    renderer: 'svg', 
+    loop: true,
+    autopaly: true,
+    path: '../source/lottie/logo.json', 
+});
+
+// 메인비주얼 패럴렉스
+$(document).on('mousemove', function(e) {
+    $('.main_visual .person img').each(function() {
+        const pos = $(this).data('value');
+        const x = (window.innerWidth - e.pageX * pos) / 90;
+        const y = (window.innerHeight - e.pageY * pos) / 90;
+        $(this).css('transform', `translateX(${x}px) translateY(${y}px)`);
+    });
+});
+
+
 // 스킬바 숫자 바꾸기
 const skill = document.querySelectorAll('.skill_box .text > div');
 skill.forEach((el) => {
@@ -28,9 +48,10 @@ const pcS1 = $('.case1 .main_screen .screen');
 const pcM1 = $('.case1 .main_screen .mask');
 const pcS2 = $('.case2 .main_screen .screen');
 const pcM2 = $('.case2 .main_screen .mask');
+const pcS3 = $('.case3 .main_screen .screen');
+const pcM3 = $('.case3 .main_screen .mask');
 const mS1 = $('.case2 .main_mobile .mask .screen');
 const mM1 = $('.case2 .main_mobile .mask');
-console.log(mM1, mS1);
 
 // up 함수
 const aniup = function(mask, screen) {
@@ -55,6 +76,8 @@ ScrollTrigger.create({
         pcM1.mouseleave(function() { anidown(pcS1); });
         pcM2.mouseenter(function() { aniup(pcM2, pcS2); });
         pcM2.mouseleave(function() { anidown(pcS2); });
+        pcM3.mouseenter(function() { aniup(pcM3, pcS3); });
+        pcM3.mouseleave(function() { anidown(pcS3); });
         // 내려가는 화면이 제일 위에 올라와 있어야 함!! 
         mM1.mouseenter(function() { aniup(mM1, mS1); });
         mM1.mouseleave(function() { anidown(mS1); });
